1fd30f77841d35b98e0a3552a4bee70a588807b2
Malcolm Barrett <malcolmbarrett@gmail.com>
2022-09-20 16:22:29 -0400

    Initial commit

---
+++ R/tidysmd-rexports.R
@@ -0,0 +1,15 @@
+# re-export functions from {tidysmd}
+
+#' @aliases tidysmd
+#' @importFrom tidysmd geom_love
+#' @export
+tidysmd::geom_love
+#' @aliases tidysmd
+#' @importFrom tidysmd love_plot
+#' @export
+tidysmd::love_plot
+#' @aliases tidysmd
+#' @importFrom tidysmd tidy_smd
+#' @export
+tidysmd::tidy_smd
+
